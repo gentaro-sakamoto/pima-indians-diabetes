cf45c7ecb67c7ed8d891173956781e06e2c2d6f9
Gentarou Sakamoto <gentaro.sakamoto@gmail.com>
2017-07-11 18:30:29 +0900

    Add mdethod splitDataset.

--- predict_with_naive_bayes.py
+++ predict_with_naive_bayes.py
@@ -1,11 +1,22 @@
 import csv
+import random
 
 def loadCsv(filename):
-  rows = csv.reader(open(filename, 'rb'))
+  rows = csv.reader(open(filename, 'r'))
   dataset = list(rows)
   for i in range(len(dataset)):
     dataset[i] = [float(x) for x in dataset[i]]
-
   return dataset
 
-loadCsv('pima-indians-diabetes.csv')
+def splitDataset(dataset, splitRatio):
+  trainSize = int(len(dataset) * splitRatio)
+  trainSet = []
+  copiedDataSet = list(dataset)
+  while len(trainSet) < trainSize:
+    index = random.randrange(len(copiedDataSet))
+    trainSet.append(copiedDataSet.pop(index))
+  return [trainSet, copiedDataSet]
+
+train, test = splitDataset(loadCsv('pima-indians-diabetes.csv'), 0.6)
+print(train)
+print(test)
